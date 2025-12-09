d30c1054233ab033a384a24d21ba99af0d4065ea
cemilpekel <pekelsdet@gmail.com>
2025-12-08 22:42:17 -0600

    Initial commit

---
+++ src/test/java/day21/FirstTestCase.java
@@ -0,0 +1,28 @@
+package day21;
+
+import org.openqa.selenium.chrome.*;
+
+public class FirstTestCase {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testing...");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.opencart.com/index.php?route=cms/demo");
+		String actualTitle = driver.getTitle();
+		
+		if(actualTitle.equals("OpenCart - Demo")) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		
+		
+		
+		
+	}
+
+}
